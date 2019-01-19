7abd03b69b008c9304f8d2f90a6698c45d03baca
walterzhang_mac <zhihao9@ualberta.ca>
2019-01-19 14:39:25 -0700

    figure out input

--- Assignment_1.c
+++ Assignment_1.c
@@ -1,7 +1,6 @@
 
-
-
-
+#include <stdio.h>
+#include <unistd.h>
 
 
 
@@ -11,13 +10,40 @@ void printpid(){
   printf("Own pid :  %d\n", pid );
 }
 
+int enter_target_pid(){
+  int target_int_pid;
+  char target_pid[100];
+  printf("%s\n", "please enter target pid :");
+  fgets(target_pid,100,stdin);
+  sscanf(target_pid, "%d", &target_int_pid);
+  printf("The target pid you enter is : %d\n", target_int_pid);
+  return target_int_pid;
+}
+
+
+void operation(int target_pid){
+  char msn[100];
+  int pid = target_pid;
+  printf("opeartion pid :%d\n",pid );
+
+  while(1){
+  printf("Enter the message: ");
+  fgets(msn,100,stdin);
+  printf("Your message is : %s",msn);
+}
+
+
+
+
+
+
+}
 
 int main(){
-  int *target_pid;
-  printpid();
-  printf("%s\n", "please enter target pid :");
-  scanf("%d", target_pid);
-  printf("The target pid you enter is : %d\n", *target_pid );
 
+  int target_pid;
+  printpid();
+  target_pid = enter_target_pid();
+  operation(target_pid);
   return 0;
 }
