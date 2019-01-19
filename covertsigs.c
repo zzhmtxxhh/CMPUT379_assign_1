93fc1831f6257ef5d01b87961235b47f3bbe707d
Amy Mi <amy@Amys-MacBook-Pro.local>
2019-01-19 16:36:26 -0700

    Added covertsig gile

---
+++ covertsigs.c
@@ -0,0 +1,75 @@
+#include <sys/types.h>
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+volatile sig_atomic_t stop;
+
+static volatile int signalPid = -1;
+void get_pid(int sig, siginfo_t *info, void *context){
+    signalPid = info->si_pid;
+}
+
+void sig_handler(int signo){
+ if (signo == SIGUSR1){
+     printf("Signal caught\n");
+     fflush(NULL);
+     //stop = 1;
+ }
+}
+
+void printPID(){
+    int pid = getpid();
+    printf("Own PID: %d\n", pid);
+}
+
+void send_signal (int pid){
+    int ret;
+    ret = kill(pid, SIGUSR1);
+    printf("ret : %d",ret);
+
+}
+
+static void HandleHostSignal(void)
+{
+    struct sigaction sa;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_handler = sig_handler;
+    sigaction(SIGUSR1, &sa, NULL);
+}
+
+int main(){
+    printPID();
+    int target_pid;
+
+    
+    HandleHostSignal();
+    scanf("%d", &target_pid);
+    printf("%d\n", target_pid);
+    send_signal(target_pid);
+    
+    //raise(SIGUSR1);
+    //send_signal(target_pid);
+    // if (signal(SIGUSR1, sig_handler) == SIG_ERR){
+    //     printf("Can't catch SIGUSR1\n");
+    // }
+
+
+    while(1){
+        sleep(1);
+    }                        //wait for a signal
+    //printf("PID of signal sender = %d\n", signalPid);
+    // sigemptyset(&sa.sa_mask);
+    // sa.sa_flags = SA_RESTART;
+
+
+    // signal(SIGUSR1, sig_handler);
+    // //kill(target_pid, SIGUSR1);
+
+
+
+
+    return 0;
+}
