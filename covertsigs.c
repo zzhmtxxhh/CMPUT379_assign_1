8b31f27a78ea68509b0080b516ad9b5ee7e09773
User Account <siyuamymi@gmail.com>
2019-01-31 18:43:49 -0700

    Finished single signal

--- covertsigs.c
+++ covertsigs.c
@@ -12,7 +12,7 @@
 
 //#define MAX_INPUT 4096
 
-
+int flag = 0;
 char *incoming_binary[8];
 char *incoming_string[1024];
 
@@ -33,30 +33,69 @@ void printPID(){
 }
 
 
+// void sigHandler(int signo){
+//     if (signo == SIGUSR1){
+//         //printf("recieved SIGUSR1 = 0\n");
+//         //printf("PID of signal = %d\n", signalPid);
+//         ++counter_usr1;
+//         strcat(incoming_binary, "0");
+//     }
+//     else if(signo == SIGUSR2){
+//         //printf("recieved SIGUSR2 = 1\n");
+//         //printf("PID of signal = %d\n", signalPid);
+//         ++counter_usr2;
+//         strcat(incoming_binary, "1");
+//     }
+//     if (strlen(incoming_binary)>= 8){
+//     // printf("%s\n", incoming_binary);
+//     // printf("%s\n", "strtol");
+//         char c = strtol(incoming_binary, 0, 2);
+//         printf("%c", c);
+    
+//         fflush(stdout);
+//         strcat(incoming_string,&c);
+//         memset(incoming_binary, 0, 8);
+//   }
+// }
+
 void sigHandler(int signo){
-    if (signo == SIGUSR1){
-        //printf("recieved SIGUSR1 = 0\n");
-        //printf("PID of signal = %d\n", signalPid);
-        ++counter_usr1;
-        strcat(incoming_binary, "0");
+    if(signo == SIGUSR1){
+        signal_counter++;
+        printf("signal_counter %d\n", signal_counter);
+        if (signal_counter == 8){
+            printf("%s\n", "enter 7");
+            ualarm(500000, 500000);
+            signal_counter = 0;
+
+        }
     }
-    else if(signo == SIGUSR2){
-        //printf("recieved SIGUSR2 = 1\n");
-        //printf("PID of signal = %d\n", signalPid);
-        ++counter_usr2;
-        strcat(incoming_binary, "1");
-    }
-    if (strlen(incoming_binary)>= 8){
-    // printf("%s\n", incoming_binary);
-    // printf("%s\n", "strtol");
+    if (strlen(incoming_binary)== 8){
+        printf("incoming binary: &s\n", incoming_binary);
         char c = strtol(incoming_binary, 0, 2);
-        printf("%c", c);
+        printf("################     %c      ###############\n", c);
     
         fflush(stdout);
         strcat(incoming_string,&c);
+        fflush(stdout);
         memset(incoming_binary, 0, 8);
   }
+
+    if (signo == SIGALRM ){
+        flag = 1;
+
+        if (signal_counter == 1){
+            printf("Received %s\n", "0");
+            strcat(incoming_binary, "0");
+        }
+        else if( signal_counter == 5){
+            printf("received %s\n", "1");
+            strcat(incoming_binary, "1");
+        }
+        signal_counter = 0;
+    }
+
 }
+
 
 
 void sendSignal(int pid, int signo){
@@ -67,7 +106,7 @@ void sendSignal(int pid, int signo){
     else if (signo == SIGUSR2){
         ret = kill(pid, SIGUSR2);
     }
-    printf("ret: %d\n", ret);
+    //printf("ret: %d\n", ret);
 }
 
 
@@ -91,8 +130,10 @@ char* stringToBinary(char* s) {
 
 static void HandleHostSignal(void){
     struct sigaction sa;
+
     sa.sa_handler = sigHandler;
     sigemptyset(&sa.sa_mask); //clear/initialize the sa mask, which signals to block, set to none
+
     sa.sa_flags = SA_SIGINFO;
     //act.sa_flags |= SA_RESTART;
     sigaction(SIGUSR1, &sa, NULL);
@@ -100,10 +141,45 @@ static void HandleHostSignal(void){
 
 }
 
+void eightBit(int pid){
+    usleep(249125);
+    sendSignal(pid, SIGUSR1);
+    usleep(250);
+    sendSignal(pid, SIGUSR1);
+    usleep(250);
+    sendSignal(pid, SIGUSR1);
+    usleep(250);
+    sendSignal(pid, SIGUSR1);
+    usleep(250);
+    sendSignal(pid, SIGUSR1);
+    usleep(250);
+    sendSignal(pid, SIGUSR1);
+    usleep(250);
+    sendSignal(pid, SIGUSR1);
+    usleep(250);
+    sendSignal(pid, SIGUSR1);
+    usleep(249125);
+}
+
+static void HandleAlrmSignal(void){
+    struct sigaction sa;
+    sigset_t set;
+    sigaddset(&set, SIGUSR1);
+    sa.sa_handler = sigHandler;
+    sigemptyset(&sa.sa_mask); //clear/initialize the sa mask, which signals to block, set to none
+    sa.sa_mask = set;
+    sa.sa_flags = SA_SIGINFO;
+    //act.sa_flags |= SA_RESTART;
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGALRM, &sa, NULL);
+    // sigaction(SIGUSR2, &sa, NULL);
+
+}
+
 
 int main(int argc, char *argv[]){
     fflush(stdout);
-    HandleHostSignal();
+    HandleAlrmSignal();
     
 
     printPID();
@@ -119,13 +195,8 @@ int main(int argc, char *argv[]){
         //printf("1st while loop\n");
         char buf_in[MAX_INPUT];
         while(1){
-            //sleep(1);
-            //while((getchar())!= '\n');
-            //printf("2nd while loop\n");
-            //scanf("%s", buf_in);
             fgets(buf_in, MAX_INPUT, stdin);
-            //getchar();
-            //printf("after scanf\n");
+
         
             
             if(errno == EINTR) {
@@ -141,25 +212,43 @@ int main(int argc, char *argv[]){
 
 
             printf("Output: %s\n", buf_in);
-            
+            printf("%s\n", "reset alarm");
+            eightBit(target_pid);
+            usleep(500000);
 
             size_t length = strlen(buf_in);
-            //printf("%zu\n", length);
-            //printf("%s\n", stringToBinary(buf_in));
 
+            printf("error %s\n", "what happen");
+            usleep(500000);
             char* binary = stringToBinary(buf_in);
 
             for (int i = 0; i < length*8; i++){
                 char b = (char)binary[i];
                 if(b == '0'){
+                    usleep(250000);
                     sendSignal(target_pid, SIGUSR1);
-                    printf("sent SIGUSR1 = 0\n");
-                    usleep(200);
+                    printf("sent SIGUSR1\n");
                     fflush(stdout);
+
+                    
+                    usleep(250000);
+
+                    
                 }else if (b == '1'){
-                    sendSignal(target_pid, SIGUSR2);
-                    printf("sent SIGUSR2 = 1\n");
-                    usleep(200);
+                    usleep(249000);
+                    sendSignal(target_pid, SIGUSR1);
+                    usleep(500);
+                    sendSignal(target_pid, SIGUSR1);
+                    usleep(500);
+                    sendSignal(target_pid, SIGUSR1);
+                    usleep(500);
+                    sendSignal(target_pid, SIGUSR1);
+                    usleep(500);
+                    sendSignal(target_pid, SIGUSR1);
+                    usleep(249000);
+
+                    printf("sent 5 SIGUSR1\n");
+                    
                     fflush(stdout);
                 }
             }
